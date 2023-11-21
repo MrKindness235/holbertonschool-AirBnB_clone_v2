ccb58a04446910ae67ab8d43c142bd6a801f4ebf
DiegoPla1104 <diegopla1104@gmail.com>
2023-11-21 12:29:30 +0000

    fix: added the requirements for task 2

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +115,34 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
-            print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+        try:
+            if not args:
+                raise SyntaxError
+            splargs = agrs.split(" ")
+            param = {}
+            if len(splargs) > 1:
+                for i in range(1, len(splargs)):
+                    if len(splargs[i].split("=")) == 2:
+                        key = splargs[i].split("=")[0]
+                        val = splargs[i].split("=")[1].replace("_", " ")
+                        val = val.strip('"')
+                        if val.isnumeric():
+                            val = int(val)
+                        else:
+                            try:
+                                float(val)
+                            except Exception:
+                                pass
+                        param[key] = value
+            my_object = eval(f"{splargs[0]}()")
+            for key, values in param.items():
+                setatr(my_object, key, values)
+            my_object.save()
+            print(f"{my_object.id}")
+        except SyntaxError:
+            pass
+        except NameError:
+            pass
 
     def help_create(self):
         """ Help information for the create method """
@@ -264,7 +282,7 @@ class HBNBCommand(cmd.Cmd):
             return
 
         # first determine if kwargs or args
-        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) is dict:
+        if '{' in args[2] and '}' in args[2] and type(eval(args[2])) == dict:
             kwargs = eval(args[2])
             args = []  # reformat kwargs into list, ex: [<name>, <value>, ...]
             for k, v in kwargs.items():
@@ -272,7 +290,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +298,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
