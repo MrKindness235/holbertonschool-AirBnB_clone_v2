54045873ea08d19a548d9e11e52c7eb9c1776620
DiegoPla1104 <diegopla1104@gmail.com>
2023-11-21 14:52:02 +0000

    fix: wrong quotes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,5 @@
 -- Script to prepare a MySQL server for the project
 CREATE DATABASE hbnb_dev_db IF NOT EXISTS;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON *hbnb_dev_db* TO 'hbnb_dev'@'localhost'
-GRANT SELECT PRIVILEGE ON *performance_schema* TO 'hbnb_dev'@'localhost'
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
