349ed6c18a401a316a01fca1eae78093e84ba397
DiegoPla1104 <diegopla1104@gmail.com>
2023-11-27 02:59:49 +0000

    fi: oss

--- models/state.py
+++ models/state.py
@@ -10,7 +10,7 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    if getenv("HBNB_TYPE_STORAGE") is "db":
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         name = Column(String(128), nullable=False)
         cities = relationship('City', cascade='delete', backref='state')
     else:
