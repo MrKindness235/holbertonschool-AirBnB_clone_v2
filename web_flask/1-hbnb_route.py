350dc537a9407b3ba1a8f63dc53654581f03d14f
MrKindness235 <patriciocoitino235@gmail.com>
2023-12-11 08:11:12 -0800

    Upgrades, people, upgrades

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
 This is a major update to 0.
+It also displays hbnb.
+Both listen to 0.0.0.0, port 5000
 """
 
 from flask import Flask
@@ -8,12 +10,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
-def hello_world():
+def hello_hbnb():
     """Hello function that displays:"""
     return "<p>Hello HBNB!</p>"
 
 @app.route("/hbnb", strict_slashes=False)
-def hello_world():
+def hbnb():
     """Function that displays:"""
     return "<p>HBNB</p>"
 
