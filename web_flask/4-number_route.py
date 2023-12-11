6ce5e87a48633affceebc2a4e7d9f05355ed3b8c
MrKindness235 <patriciocoitino235@gmail.com>
2023-12-11 13:50:09 -0800

    Fifth task done.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+This is a major update to 0.
+It also displays hbnb.
+Added print_C function.
+Both listen to 0.0.0.0, port 5000.
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Hello function that displays:"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def print_hbnb():
+    """Function that displays:"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def print_C(text):
+    """Function that displays:"""
+    return "C {}".format(escape(text).replace('_', ' '))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def print_python(text='is cool'):
+    """Function that displays:"""
+    return "Python {}".format(escape(text).replace('_', ' '))
+
+
+@app.route("/python/<int:n>", strict_slashes=False)
+def print_number(n):
+    """Function that displays:"""
+    return "{} is a number".format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
