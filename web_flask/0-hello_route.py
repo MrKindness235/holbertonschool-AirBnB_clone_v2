18706b6afb368ee10328d2a37b1be2582a2343f5
MrKindness235 <patriciocoitino235@gmail.com>
2023-12-11 08:02:38 -0800

    Documentation!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This code creates an intance of Flask and makes
+it listen to 0.0.0.0, port 5000.
+It returns "Hello HBNB!" as display.
+"""
 
 from flask import Flask
 
@@ -6,6 +11,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """Main function"""
     return "<p>Hello HBNB!</p>"
 
 if __name__ == '__main__':
